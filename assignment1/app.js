2315810de0017d2314514123056e3eb28b31b431
Efraim <efraimbelga@gmail.com>
2020-12-08 19:08:48 +0800

    try2

--- assignment1/app.js
+++ assignment1/app.js
@@ -30,13 +30,16 @@
 
 	.controller('DIController', DIController);
 
-	function DIController($scope, $filter){
+	function DIController($scope, $filter, $injector){
 		$scope.name = 'Kiams';
 
 		$scope.upper = function(){
 			var upCase = $filter('uppercase');
 			$scope.name = upCase($scope.name)
 		}
+
+		console.log($injector.annotate(DIController))
+
 	}
 
 	function AnnotateMe(name, job, blah){
