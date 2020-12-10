131155980eb24623130464eb866ca778f3c6766a
WFH <WFH@COSCEWKS00743.LEGAZPI.INNODATA.NET>
2020-12-10 10:04:56 +0800

    My assignment1

--- assignment1/app.js
+++ assignment1/app.js
@@ -9,26 +9,40 @@
 	function assignmentController($scope){
 		$scope.meals = "";
 		$scope.stateofbeing = "";
-
+		
 
 		$scope.checkMeal = function(){
-			$scope.stateofbeing = getresult($scope.meals);
+			var message = "";
+			var blank = 0;
+			var meal = $scope.meals;
+			meal = meal.replace(/,\s*$/, "");
+			var food = removeComma(meal);
+			if(food === ""){
+				message= "Please enter data first";
+			}else{
+				var foods = meal.split(",");
+				for (var i = 0; i < foods.length; i++) {
+				  	food = removeComma(foods[i]);
+				  	if(food === ""){
+				  		blank++;
+				  		message = 'Empty';
+				  		break;
+				  	}
+				}
+				if(blank<=0){
+					if(foods.length > 3){
+						message = 'Too much!';
+					}else{
+						message = "Enjoy!"
+					}
+				}			
+			}			
+			$scope.stateofbeing = message;
 		};
 
-		function getresult(string){
-			var totalMeal = 0;
-			var res = string.split(",");
-			var message = "";
-
-			if(res.length > 0){
-
-			}else{
-				message = ""
-			}
-			for(var i= 0; i<string.length; i++){
-				totalStringValue += string.charCodeAt(i);
-			}
-			return message;
+		function removeComma(string){
+			var str = string.replace(/\s/g, '');
+			return str;
 		}
 	}
 	
