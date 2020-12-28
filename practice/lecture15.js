4343a2c0ac40b8a67c0794edb711e8f8b81b7eab
Belga <YH4@sgpcloud.innodata.net>
2020-12-28 11:42:42 +0800

    LECTURE 15

---
+++ practice/lecture15.js
@@ -0,0 +1,36 @@
+(function(){
+	'use strict';
+
+	angular.module('counterApp', [])
+
+	.controller('CounterController', CounterController);
+
+	CounterController.$inject = ['$scope', '$timeout'];
+	function CounterController($scope, $timeout){
+		$scope.counter = 0;
+	
+		// $scope.upCounter = function(){
+		// 	setTimeout(function(){
+		// 		$scope.counter ++;
+		// 		console.log("Counter Incremented");
+		// 		$scope.$digest();
+		// 	}, 2000)
+		// }
+
+		// $scope.upCounter = function(){
+		// 	setTimeout(function(){
+		// 		$scope.$apply(function(){
+		// 			$scope.counter ++;
+		// 			console.log("Counter Incremented");
+		// 		});
+		// 	}, 2000);
+		// }
+
+		$scope.upCounter = function(){
+			$timeout(function(){
+				$scope.counter ++;
+				console.log("Counter Incremented");
+			}, 2000)
+		}
+	}	
+})();
